a7e42888fc99e6ab7686086e0a6e71c420547072
natnaelLake <bewuketulake1212@gmail.com>
2023-06-02 10:34:18 +0300

    intergration of graphql and mongodb is done.

--- back-end/Schema/Schema.js
+++ back-end/Schema/Schema.js
@@ -1,7 +1,8 @@
 const graphql = require("graphql");
 const _ = require('lodash')
-const { GraphQLObjectType, GraphQLString ,GraphQLID,GraphQLInt} = graphql;
-
+const { GraphQLObjectType, GraphQLString,GraphQLNonNull ,GraphQLID,GraphQLInt} = graphql;
+const Books = require('../models/Books')
+const Authors = require('../models/Authors')
 const books = [
   {
     name: "name 1",
@@ -73,7 +74,8 @@ const BookType = new GraphQLObjectType({
     author:{
         type:AuthorType,
         resolve(parent,args){
-            return _.find(authors,{id:parent.authorId})
+            // return _.find(authors,{id:parent.authorId})
+            return Authors.findById(parent.authorId)
         }
     }
   }),
@@ -88,7 +90,8 @@ const AuthorType = new GraphQLObjectType({
         books:{
             type:new graphql.GraphQLList(BookType),
             resolve(parent,args){
-                return _.filter(books,{authorId:parent.id})
+                // return _.filter(books,{authorId:parent.id})
+                return Books.findById({authorId:parent.id});
             }
         }
     })
@@ -100,34 +103,72 @@ const RoootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(books,{id:args.id})
+        // return _.find(books,{id:args.id})
+        return Books.findById(args.id)
       },
     },
     author:{
       type:AuthorType,
       args:{id:{type:GraphQLID}},
       resolve(parent,args){
-          return _.find(authors,{id:args.id})
+          // return _.find(authors,{id:args.id})
+          return Authors.findById(args.id)
       }
     },
     books:{
         type: new graphql.GraphQLList(BookType),
         resolve(parent,args){
-            return books
+            return Books.find({})
         }
     },
     authors:{
         type: new graphql.GraphQLList(AuthorType),
         resolve(parent,args){
-            return authors
+            return Authors.find({})
         }
     }
   }
 });
 
+const Mutation = new GraphQLObjectType({
+  name:'Mutation',
+  fields:{
+    addAuthor:{
+      type:AuthorType,
+      args:{
+        name:{type:new GraphQLNonNull(GraphQLString)},
+        age:{type:new GraphQLNonNull(GraphQLInt)}
+      },
+      resolve(parent,args){
+        let author = Authors.create({
+          name:args.name,
+          age:args.age
+        })
+        return author
+      }
+    },
+    addBook:{
+      type:BookType,
+      args:{
+        name:{type:new GraphQLNonNull(GraphQLString)},
+        genere:{type:new GraphQLNonNull(GraphQLString)},
+        authorId:{type:new GraphQLNonNull(GraphQLID)}
+      },
+      resolve(parent,args){
+        let book = Books.create({
+          name:args.name,
+          genere:args.genere,
+          authorId:args.authorId
+        })
+        return book
+      }
+    }
+  }
+})
 // book(id){
 
 // }
 module.exports = new graphql.GraphQLSchema({
   query: RoootQuery,
+  mutation:Mutation
 });
--- back-end/models/Authors.js
+++ back-end/models/Authors.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const authorDb = mongoose.Schema({
     name:String,
     age:Number,
-})
+},{timestamps:true})
 
 module.exports = mongoose.model('Author',authorDb)
